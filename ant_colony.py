21f57c449b551c571bf470351391af70832f5bca
Andrey Tatarinov <a@tatarinov.co>
2021-03-30 18:48:20 +0300

    migrate to ECS

--- ant_colony.py
+++ ant_colony.py
@@ -2,145 +2,229 @@ from typing import Tuple, Any, Optional
 from numpy.random import randint, random
 import numpy as np
 import math
+from pygame.transform import rotate
+from pygame.version import ver
 import skimage.draw
 from scipy.signal import convolve2d
+
+import esper
 
 import pygame as pg
 from pygame import gfxdraw
 from pygame.math import Vector2
 
 N_SCENTS = 2
-IDX_SCENT_TO_HOME = 0
-IDX_SCENT_TO_FOOD = 1
+SCENT_TO_HOME = 0
+SCENT_TO_FOOD = 1
 
 SCENT_DECAY = 0.995
 
 
 ANT_VELOCITY = 1.
 ANT_ROTATION_VEL = 60
-CONTACT_DISTANCE = 20
+CONTACT_DISTANCE = 5
 SNIFF_DISTANCE = 30.
 
 
 STATE_SEARCHING_FOOD = 'search_food'
 STATE_GOING_HOME = 'going_home'
+STATE_AGE = 1000
 
 
 FOOD_SIZE = 50
 N_FOODS = 20
 
+N_ANTS = 100
 
 WORLD_SIZE = Vector2(500, 500)
 
 
-class Ant:
+class Movable:
     def __init__(self, position: Vector2, velocity: Vector2):
         self.position = position
         self.velocity = velocity
 
-        self.state = STATE_SEARCHING_FOOD
-        self.state_age = 0
-    
-    def sniff(self, world: 'World') -> Tuple[Any, Any]:
-        center_dir = self.position + (self.velocity * SNIFF_DISTANCE)
-        left_dir = self.position + (self.velocity.rotate(-60) * SNIFF_DISTANCE)
-        right_dir = self.position + (self.velocity.rotate(60) * SNIFF_DISTANCE)
+
+class Renderable:
+    def __init__(self, size, color):
+        self.size = size
+        self.color = color
 
 
-        left_mask = skimage.draw.polygon(
-            [int(self.position.x), int(center_dir.x), int(left_dir.x)],
-            [int(self.position.y), int(center_dir.y), int(left_dir.y)],
-            (int(world.size.x), int(world.size.y))
-        )
-
-        right_mask = skimage.draw.polygon(
-            [int(self.position.x), int(center_dir.x), int(right_dir.x)],
-            [int(self.position.y), int(center_dir.y), int(right_dir.y)],
-            (int(world.size.x), int(world.size.y))
-        )
-
-        left = world.scents[left_mask[0], left_mask[1], :].sum(0)
-        right = world.scents[right_mask[0], right_mask[1], :].sum(0)
-
-        return left, right
-
-    def tick(self, world: 'World') -> None:
-        self.state_age += 1
-
-        is_home = world.is_home(self.position)
-        food = world.get_food(self.position)
-
-        if self.state == STATE_SEARCHING_FOOD:
-            if is_home:
-                self.state_age = 0
-
-            if food is not None:
-                food.size -= 1
-                self.state=STATE_GOING_HOME
-
-                self.velocity = self.velocity.rotate(180)
-                self.state_age = 0
-
-        elif self.state == STATE_GOING_HOME:
-            if food is not None:
-                self.state_age = 0
-
-            if is_home:
-                self.state = STATE_SEARCHING_FOOD
-                self.velocity = self.velocity.rotate(180)
-                self.state_age = 0
+class Stinky:
+    def __init__(self, size: int, scent: int = None):
+        self.size = size
+        self.scent = scent
 
 
-        if self.state == STATE_SEARCHING_FOOD:
-            if self.state_age < 1000:
-                world.leave_scent(self.position, IDX_SCENT_TO_HOME)
-            sniff_for = IDX_SCENT_TO_FOOD
-        elif self.state == STATE_GOING_HOME:
-            if self.state_age < 1000:
-                world.leave_scent(self.position, IDX_SCENT_TO_FOOD)
-            sniff_for = IDX_SCENT_TO_HOME
-
-
-        l, r = self.sniff(world)
-        l = l[sniff_for]
-        r = r[sniff_for]
-
-        self.velocity.rotate_ip((randint(-ANT_ROTATION_VEL, 0) * (l+1) + randint(0, ANT_ROTATION_VEL) * (r+1)) / (l+r+2) + randint(-ANT_ROTATION_VEL/4, ANT_ROTATION_VEL/4))
-
-        self.position += self.velocity
-
-        self.position.x = min(max(0, self.position.x), world.size.x - 1)
-        self.position.y = min(max(0, self.position.y), world.size.y - 1)
+class Home:
+    pass
 
 
 class Food:
-    def __init__(self, position, size):
-        self.position = position
+    def __init__(self, size):
         self.size = size
 
 
-class World:
-    def __init__(self, size: Vector2, n_ants, n_foods) -> None:
-        self.size = size
+class Ant:
+    def __init__(self):
+        self.state = STATE_SEARCHING_FOOD
+        self.state_age = 0
 
-        self.home_pos = size.elementwise() * Vector2(random(), random())
 
-        self.foods = [
-            Food(size.elementwise() * Vector2(random(), random()), FOOD_SIZE)
-            for i in range(n_foods)
-        ]
+class MovementProcessor(esper.Processor):
+    def __init__(self, world_size: Vector2):
+        super().__init__()
+        self.world_size = world_size
+    
+    def process(self):
+        for ent, mov in self.world.get_component(Movable):
+            mov.position = mov.position + mov.velocity
 
-        self.ants = [
-            Ant(Vector2(self.home_pos), Vector2(1,0).rotate(randint(0, 360)))
-            for i in range(n_ants)
-        ]
+            mov.position.x = min(max(0, mov.position.x), self.world_size.x - 1)
+            mov.position.y = min(max(0, mov.position.y), self.world_size.y - 1)
 
-        self.scents = np.zeros((int(size.x), int(size.y), N_SCENTS))
 
-    def draw(self) -> Any:
-        scents_arr = np.zeros((int(self.size.x), int(self.size.y), 3))
-        scents_arr += self.scents[:,:,[IDX_SCENT_TO_HOME]] * np.array([0, 200, 0])
-        scents_arr += self.scents[:,:,[IDX_SCENT_TO_FOOD]] * np.array([0, 0, 200])
+class FoodProcessor(esper.Processor):
+    def process(self):
+        for ent, (food, ren, stinky) in self.world.get_components(Food, Renderable, Stinky):
+            if food.size > 0:
+                stinky.size = int(food.size / 10)
+                ren.size = int(food.size / 10)
+            else:
+                self.world.delete_entity(ent)
+
+
+class AntProcessor(esper.Processor):
+    def __init__(self, scent_processor) -> None:
+        super().__init__()
+        self.scent_processor = scent_processor
+
+    def get_food(self, pos: Vector2) -> Optional[Food]:
+        for ent, (food, mov) in self.world.get_components(Food, Movable):
+            if pos.distance_to(mov.position) < CONTACT_DISTANCE and food.size > 0:
+                return food
+        return None
+
+    def is_home(self, pos: Vector2) -> bool:
+        for ent, (home, mov) in self.world.get_components(Home, Movable):
+            if pos.distance_to(mov.position) < CONTACT_DISTANCE:
+                return True
+        return False
+
+    def sniff(self, mov: Movable) -> Tuple[Any, Any]:
+        center_dir = mov.position + (mov.velocity * SNIFF_DISTANCE)
+        left_dir = mov.position + (mov.velocity.rotate(-60) * SNIFF_DISTANCE)
+        right_dir = mov.position + (mov.velocity.rotate(60) * SNIFF_DISTANCE)
+
+
+        left_mask = skimage.draw.polygon(
+            [int(mov.position.x), int(center_dir.x), int(left_dir.x)],
+            [int(mov.position.y), int(center_dir.y), int(left_dir.y)],
+            (int(self.scent_processor.world_size.x), int(self.scent_processor.world_size.y))
+        )
+
+        right_mask = skimage.draw.polygon(
+            [int(mov.position.x), int(center_dir.x), int(right_dir.x)],
+            [int(mov.position.y), int(center_dir.y), int(right_dir.y)],
+            (int(self.scent_processor.world_size.x), int(self.scent_processor.world_size.y))
+        )
+
+        left = self.scent_processor.scents[left_mask[0], left_mask[1], :].sum(0)
+        right = self.scent_processor.scents[right_mask[0], right_mask[1], :].sum(0)
+
+        return left, right
+
+    def process(self):
+        for ent, (ant, mov, stinky) in self.world.get_components(Ant, Movable, Stinky):
+            ant.state_age += 1
+
+            if ant.state_age < STATE_AGE:
+                if ant.state == STATE_GOING_HOME:
+                    stinky.scent = SCENT_TO_FOOD
+                elif ant.state == STATE_SEARCHING_FOOD:
+                    stinky.scent = SCENT_TO_HOME
+            else:
+                stinky.scent = None
+
+            food = self.get_food(mov.position)
+            is_home = self.is_home(mov.position)
+
+            if ant.state == STATE_SEARCHING_FOOD:
+                if is_home:
+                    ant.state_age = 0
+
+                if food is not None:
+                    food.size -= 1
+                    ant.state=STATE_GOING_HOME
+
+                    mov.velocity = mov.velocity.rotate(180)
+                    ant.state_age = 0
+
+            elif ant.state == STATE_GOING_HOME:
+                if food is not None:
+                    ant.state_age = 0
+
+                if is_home:
+                    ant.state = STATE_SEARCHING_FOOD
+                    mov.velocity = mov.velocity.rotate(180)
+                    ant.state_age = 0
+
+
+            if ant.state == STATE_SEARCHING_FOOD:
+                sniff_for = SCENT_TO_FOOD
+            elif ant.state == STATE_GOING_HOME:
+                sniff_for = SCENT_TO_HOME
+
+
+            l, r = self.sniff(mov)
+            l = l[sniff_for]
+            r = r[sniff_for]
+
+            mov.velocity.rotate_ip((randint(-ANT_ROTATION_VEL, 0) * (l+1) + randint(0, ANT_ROTATION_VEL) * (r+1)) / (l+r+2))
+            mov.velocity.rotate_ip(randint(-ANT_ROTATION_VEL/4, ANT_ROTATION_VEL/4))
+
+
+class ScentProcessor(esper.Processor):
+    def __init__(self, world_size) -> None:
+        super().__init__()
+
+        self.world_size = world_size
+        self.scents = np.zeros((int(world_size.x), int(world_size.y), N_SCENTS))
+
+    def leave_scent(self, position, size, scent) -> None:
+        cooords = skimage.draw.circle(int(position.x), int(position.y), size, (int(self.world_size.x), int(self.world_size.y)))
+
+        # self.scents[int(position.x), int(position.y), scent] += 1
+        self.scents[cooords[0], cooords[1], scent] = 1
+    
+    def process(self):
+        decay_mat = np.array([
+            [0.00, 0.001,  0.00,],
+            [0.001,  0.992,  0.001   ],
+            [0.00, 0.001,  0.00,]
+        ])
+        self.scents[:,:, SCENT_TO_FOOD] = convolve2d(self.scents[:,:, SCENT_TO_FOOD], decay_mat, mode='same')
+        self.scents[:,:, SCENT_TO_HOME] = convolve2d(self.scents[:,:, SCENT_TO_HOME], decay_mat, mode='same')
+
+        for ent, (mov, stinky) in self.world.get_components(Movable, Stinky):
+            if stinky.scent is not None:
+                self.leave_scent(mov.position, stinky.size, stinky.scent)
+
+
+class RenderProcessor(esper.Processor):
+    def __init__(self, world_size, screen, scent_processor, clear_color=(0, 0, 0)):
+        super().__init__()
+        self.world_size = world_size
+        self.screen = screen
+        self.clear_color = clear_color
+
+        self.scent_processor = scent_processor
+
+    def process(self):
+        scents_arr = np.zeros((int(self.world_size.x), int(self.world_size.y), 3))
+        scents_arr += self.scent_processor.scents[:,:,[SCENT_TO_HOME]] * np.array([0, 200, 0])
+        scents_arr += self.scent_processor.scents[:,:,[SCENT_TO_FOOD]] * np.array([0, 0, 200])
 
 
         # for ant in self.ants:
@@ -148,71 +232,67 @@ class World:
         #     scents_arr[l[0], l[1], :] += np.array([80, 0, 0])
         #     scents_arr[r[0], r[1], :] += np.array([120, 0, 0])
 
-
         surface = pg.surfarray.make_surface(scents_arr)
 
-        gfxdraw.filled_circle(surface, int(self.home_pos.x), int(self.home_pos.y), 10, pg.Color(0, 0, 0))
-        gfxdraw.filled_circle(surface, int(self.home_pos.x), int(self.home_pos.y), 8, pg.Color(0, 200, 0))
+        for ent, (mov, ren) in self.world.get_components(Movable, Renderable):
+            gfxdraw.filled_circle(surface, int(mov.position.x), int(mov.position.y), ren.size + 1, pg.Color(0, 0, 0))
+            gfxdraw.filled_circle(surface, int(mov.position.x), int(mov.position.y), ren.size, ren.color)
 
-        for food in self.foods:
-            gfxdraw.filled_circle(surface, int(food.position.x), int(food.position.y), int(food.size / 10), pg.Color(0, 0, 200))
-            # gfxdraw.filled_circle(surface, food.x, food.y, 4, pg.Color(0, 0, 200))
+        self.screen.fill((30, 30, 30))
+        self.screen.blit(surface, (0, 0))
 
-        for ant in self.ants:
-            gfxdraw.filled_circle(surface, int(ant.position.x), int(ant.position.y), 5, pg.Color(0, 0, 0))
-            gfxdraw.filled_circle(surface, int(ant.position.x), int(ant.position.y), 4, pg.Color(200, 0, 0))
-
-        return surface
-
-    def get_food(self, pos: Vector2) -> Optional[Food]:
-        for food in self.foods:
-            if pos.distance_to(food.position) < CONTACT_DISTANCE and food.size > 0:
-                return food
-        return None
-
-    def is_home(self, pos: Vector2) -> bool:
-        if pos.distance_to(self.home_pos) < CONTACT_DISTANCE:
-            return True
-        else:
-            return False
-
-    def tick(self) -> None:
-        decay_mat = np.array([
-            [0.00, 0.0005,  0.00,],
-            [0.0005,  0.992,  0.0005   ],
-            [0.00, 0.0005,  0.00,]
-        ])
-        self.scents[:,:, IDX_SCENT_TO_FOOD] = convolve2d(self.scents[:,:, IDX_SCENT_TO_FOOD], decay_mat, mode='same')
-        self.scents[:,:, IDX_SCENT_TO_HOME] = convolve2d(self.scents[:,:, IDX_SCENT_TO_HOME], decay_mat, mode='same')
-
-        for ant in self.ants:
-            ant.tick(self)
-
-    def leave_scent(self, position, scent) -> None:
-        self.scents[int(position.x), int(position.y), scent] += 1
+        pg.display.flip()
 
 
 def main() -> None:
-    world = World(WORLD_SIZE, 100, N_FOODS)
-
-
     pg.init()
     screen = pg.display.set_mode((int(WORLD_SIZE.x), int(WORLD_SIZE.y)))
     clock = pg.time.Clock()
 
     running = True
+
+    world = esper.World()
+
+    home = world.create_entity()
+    home_pos = WORLD_SIZE.elementwise() * Vector2(random(), random())
+    world.add_component(home, Home())
+    world.add_component(home, Stinky(10, SCENT_TO_HOME))
+    world.add_component(home, Movable(home_pos, Vector2()))
+    world.add_component(home, Renderable(10, pg.Color(0, 200, 0)))
+
+    for _ in range(N_FOODS):
+        food = world.create_entity()
+        world.add_component(food, Food(FOOD_SIZE))
+        world.add_component(food, Stinky(1, SCENT_TO_FOOD))
+        world.add_component(food, Movable(WORLD_SIZE.elementwise() * Vector2(random(), random()), Vector2()))
+        world.add_component(food, Renderable(1, pg.Color(0, 0, 200)))
+
+    for _ in range(N_ANTS):
+        ant = world.create_entity()
+        world.add_component(ant, Ant())
+        world.add_component(ant, Stinky(1, None))
+        world.add_component(ant, Movable(home_pos, Vector2(1, 0).rotate(randint(0, 360))))
+        world.add_component(ant, Renderable(3, pg.Color(200, 0, 0)))
+
+    scent_processor = ScentProcessor(WORLD_SIZE)
+    food_processor = FoodProcessor()
+    ant_processor = AntProcessor(scent_processor)
+    movement_processor = MovementProcessor(WORLD_SIZE)
+    render_processor = RenderProcessor(WORLD_SIZE, screen, scent_processor)
+
+    world.add_processor(ant_processor)
+    world.add_processor(scent_processor)
+    world.add_processor(movement_processor)
+    world.add_processor(render_processor)
+    world.add_processor(food_processor)
+
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
 
-        world.tick()
+        world.process()
 
-        surface = world.draw()
-
-        screen.fill((30, 30, 30))
-        screen.blit(surface, (0, 0))
-        pg.display.flip()
         clock.tick(60)
 
 
